ec85d76f6cb5c11e055bbb0f2f17514e3e393bba
Gordon Byers <gobyers@microsoft.com>
2020-11-10 16:25:14 +0000

    logging

--- simpleapi/app.py
+++ simpleapi/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_file, jsonify
-import requests
+import requests, os, logging
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -10,6 +10,35 @@ def index():
 @app.route('/canary')
 def canary():
     return jsonify({'success': 'true',
+                    'message': 'HTTP 200. All ok.'})
+
+@app.route('/config')
+def config():
+    # create logger
+    logger = logging.getLogger('simpleapi')
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    logger.info('os.environ')
+    logger.info(os.environ)
+
+    if 'storageFolder' in os.environ:
+        storage_folder =os.environ['storageFolder']
+    else:
+        storage_folder ='[ERROR] environment variable not found. Check how you set it.  export storageFolder="blah"'
+    return jsonify({'MandatoryEnvVariables': { 'storageFolder' : storage_folder},
                     'message': 'HTTP 200. All ok.'})
 
 @app.route('/', methods=['PUT'])
@@ -33,7 +62,18 @@ def upload_image():
 
 
 @app.route('/testimagedl', methods=['GET'])
-def create_record():
+def get_remote_image():
+    sampleimageurl='https://gordon.byers.me/assets/img/die-bart-die.png'
+    imagefile = requests.get(sampleimageurl)
+    open('/tmp/localimage.png', 'wb').write(imagefile.content)
+
+    return send_file('/tmp/localimage.png', mimetype='image/png')
+
+@app.route('/localimage', methods=['GET'])
+def get_local_image():
+    storage_folder =os.environ['storageFolder']
+    listfiles = os.listdir(storage_folder)
+
     sampleimageurl='https://gordon.byers.me/assets/img/die-bart-die.png'
     imagefile = requests.get(sampleimageurl)
     open('/tmp/localimage.png', 'wb').write(imagefile.content)
